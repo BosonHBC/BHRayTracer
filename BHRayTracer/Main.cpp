d791fb853a4fd8ce6a52b69ee899a3b6b3472a39
HUANG Bocheng <a923000213@163.com>
2019-08-25 22:03:21 -0600

    Try to add recurvise function to iterate the rootNode

--- BHRayTracer/Main.cpp
+++ BHRayTracer/Main.cpp
@@ -11,13 +11,31 @@ Sphere theSphere;
 #define PI 3.14159265
 int LoadScene(char const *filename);
 
+void recursive(Color24* _pixels, int _i, int _j,Node* root, Ray ray) {
+	if (root->GetNumChild() <= 0) return;
+	for (int i = 0; i < root->GetNumChild(); i++)
+	{
+		if (root->GetChild(i)->GetNodeObj() != nullptr) {
+			
+			Ray transformedRay = root->GetChild(i)->ToNodeCoords(ray);
+			HitInfo outHit;
+			outHit.node = root->GetChild(i);
+			if (root->GetChild(i)->GetNodeObj()->IntersectRay(transformedRay, outHit, 1))
+			{
+				_pixels[_j*camera.imgWidth + _i].Set(255, 255, 255);
+			}
+			recursive(_pixels, _i, _j,root->GetChild(i), transformedRay);
+		}
+
+	}
+}
+
 void ShowViewport();
 void BeginRender() {
-	// All rays start position
+	// Ray pre-calcuated data
 	Vec3f rayStart = camera.pos;
 	float aor = camera.imgWidth / (float)camera.imgHeight;
 	float tan_h_pov = tan(camera.fov / 2 * PI / 180.0);
-
 	float l = 1;
 	float h = 2 * l * tan_h_pov;
 	float w = aor * h;
@@ -26,6 +44,8 @@ void BeginRender() {
 	Vec3f camXAxis = camZAxis.Cross(camYAxis);
 	Vec3f topLeft = rayStart - camZAxis * l + camYAxis * h / 2 - camXAxis * w / 2;
 
+	// Node info
+
 	Sphere* sps = new Sphere[3];
 	sps[0].c = Vec3f(0, 50, -25);
 	sps[0].r = 25;
@@ -33,6 +53,8 @@ void BeginRender() {
 	sps[1].r = 5;
 	sps[2].c = Vec3f(0, 50, 11.1f);
 	sps[2].r = 1;
+
+	
 	Color24* pixels = renderImage.GetPixels();
 	renderImage.ResetNumRenderedPixels();
 	for (size_t i = 0; i < camera.imgWidth; i++)
@@ -45,6 +67,8 @@ void BeginRender() {
 			tempRay.p = rayStart;
 			Vec3f pixelPos = topLeft + (i + 1 / 2) * w / camera.imgWidth * camXAxis - (j + 1 / 2) * h / camera.imgHeight * camYAxis;
 			tempRay.dir = pixelPos - rayStart;
+
+			//recursive(pixels, i, j, &rootNode, tempRay);
 			for (size_t s = 0; s < 3; s++)
 			{
 				HitInfo outHit;
@@ -63,6 +87,7 @@ void BeginRender() {
 void StopRender() {
 
 }
+
 
 int main() {
 	const char* filename = "data/proj1.xml";
